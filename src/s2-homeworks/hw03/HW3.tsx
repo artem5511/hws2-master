ff4f8e67630ac85d1e956c88eb28261f00219434
lui viton <artem_5511@mail.ru>
2022-11-02 00:08:59 +0300

    1

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import { v1 } from 'uuid'
 import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
+import {logDOM} from '@testing-library/react';
+import user from '../hw08/User';
 
 /*
 * 1 - описать тип UserType
@@ -19,20 +21,22 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any
+    name:string // need to fix any
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = { // need to fix
-    }
+export const pureAddUserCallback = (name: string, setUsers:(user:UserType[])=>void, users: UserType[] ) => { // need to fix any
+    const user = { _id:v1(), name: name} // need to fix
     setUsers([...users, user])
 }
 
-const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
+
+const HW3 = () => {
+    const [users, setUsers] = useState<UserType[]>([]) // need to fix any
+    console.log(setUsers)
+
+    const addUserCallback = (name: string) => { // need to fix any
         pureAddUserCallback(name, setUsers, users)
     }
 
